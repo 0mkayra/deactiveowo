a21850b22e074bf2339a85c416683f77a59df254
0mkayra <93237618+0mkayra@users.noreply.github.com>
2022-03-28 00:35:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  Token: "ODg1ODgwNTYwNTUwMDMxMzkw.YkDhJA.KyT1qkGqWxmoTEEBBmuwJHW8ypE", 
+  Prefix: "!",
+  BotOwner: "885880560550031390", //kendi hesap idnizi yazın
+  OwoBotID: "408785106942164992", // Varsayılan Değiştirmeyin!
+  UserID: "", // Bu kısıma arkadaşlarınızın ID'sini yazabilirsiniz bot spam'a düşmesin diye yapıldı. arada bir ıdsi girilen kişilere etiket atar.
+  GuildID: "954365759642034176" //botun mesaj atacağı sunucu id yaz
+};
